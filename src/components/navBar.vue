b15662ebdfcf259eb1493c9aeabc32ce31a58f4c
usama-safdar541 <usama.safdar@devbatch.com>
2023-05-18 18:49:57 +0500

    updated

---
+++ src/components/navBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <b-row>
+            <b-col>
+                <div class="m-logo">
+                    <h1>KONN</h1>
+                </div>
+            </b-col>
+            <b-col>
+                <div class="navbar">
+                    <b-nav>
+                        <b-nav-item active>Active</b-nav-item>
+                        <b-nav-item>Link</b-nav-item>
+                        <b-nav-item>Another Link</b-nav-item>
+                        <b-nav-item disabled>Disabled</b-nav-item>
+                    </b-nav>
+                </div>
+            </b-col>
+            <b-col>
+                <div class="navbar">
+              <b-img src="" ></b-img>
+
+                </div>
+            </b-col>
+        </b-row>
+
+    </div>
+</template>
+<script>
+</script>
+
+<style scoped>
+.navbar {
+    display: flex;
+
+}
+</style>
